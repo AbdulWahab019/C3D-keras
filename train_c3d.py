0712307911711636c7b3e4308226f8f7c0805698
Abdul Wahab <abdulwahab0193@gmail.com>
2019-09-19 21:50:39 +0500

    Modified project according to our needs.

--- train_c3d.py
+++ train_c3d.py
@@ -1,14 +1,14 @@
 # -*- coding:utf-8 -*-
 from models import c3d_model
-from keras.optimizers import SGD,Adam
+from keras.optimizers import SGD
 from keras.utils import np_utils
 from schedules import onetenth_4_8_12
 import numpy as np
-import random
 import cv2
 import os
 import random
 import matplotlib
+
 matplotlib.use('AGG')
 import matplotlib.pyplot as plt
 
@@ -49,19 +49,19 @@ def save_history(history, result_dir):
                 i, loss[i], acc[i], val_loss[i], val_acc[i]))
         fp.close()
 
-        
-def process_batch(lines,img_path,train=True):
+
+def process_batch(lines, img_path, train=True):
     num = len(lines)
-    batch = np.zeros((num,16,112,112,3),dtype='float32')
-    labels = np.zeros(num,dtype='int')
+    batch = np.zeros((num, 16, 240, 240, 3), dtype='float32')
+    labels = np.zeros(num, dtype='int')
     for i in range(num):
         path = lines[i].split(' ')[0]
         label = lines[i].split(' ')[-1]
         symbol = lines[i].split(' ')[1]
         label = label.strip('\n')
         label = int(label)
-        symbol = int(symbol)-1
-        imgs = os.listdir(img_path+path)
+        symbol = int(symbol, 10) - 1
+        imgs = os.listdir(img_path + path)
         imgs.sort(key=str.lower)
         if train:
             crop_x = random.randint(0, 15)
@@ -71,17 +71,18 @@ def process_batch(lines,img_path,train=True):
                 img = imgs[symbol + j]
                 image = cv2.imread(img_path + path + '/' + img)
                 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                image = cv2.resize(image, (171, 128))
+                image = cv2.resize(image, (240, 240))
                 if is_flip == 1:
                     image = cv2.flip(image, 1)
-                batch[i][j][:][:][:] = image[crop_x:crop_x + 112, crop_y:crop_y + 112, :]
+
+                batch[i][j][:][:][:] = image[: + 240, : + 240, :]
             labels[i] = label
         else:
             for j in range(16):
                 img = imgs[symbol + j]
                 image = cv2.imread(img_path + path + '/' + img)
                 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                image = cv2.resize(image, (171, 128))
+                image = cv2.resize(image, (240, 240))
                 batch[i][j][:][:][:] = image[8:120, 30:142, :]
             labels[i] = label
     return batch, labels
@@ -100,7 +101,7 @@ def preprocess(inputs):
     return inputs
 
 
-def generator_train_batch(train_txt,batch_size,num_classes,img_path):
+def generator_train_batch(train_txt, batch_size, num_classes, img_path):
     ff = open(train_txt, 'r')
     lines = ff.readlines()
     num = len(lines)
@@ -110,17 +111,17 @@ def generator_train_batch(train_txt,batch_size,num_classes,img_path):
         random.shuffle(index)
         for m in range(num):
             new_line.append(lines[index[m]])
-        for i in range(int(num/batch_size)):
-            a = i*batch_size
-            b = (i+1)*batch_size
-            x_train, x_labels = process_batch(new_line[a:b],img_path,train=True)
+        for i in range(int(num / batch_size)):
+            a = i * batch_size
+            b = (i + 1) * batch_size
+            x_train, x_labels = process_batch(new_line[a:b], img_path, train=True)
             x = preprocess(x_train)
-            y = np_utils.to_categorical(np.array(x_labels), num_classes)
-            x = np.transpose(x, (0,2,3,1,4))
+            y = np_utils.to_categorical(np.array(x_labels) - 1, num_classes)
+            x = np.transpose(x, (0, 2, 3, 1, 4))
             yield x, y
 
 
-def generator_val_batch(val_txt,batch_size,num_classes,img_path):
+def generator_val_batch(val_txt, batch_size, num_classes, img_path):
     f = open(val_txt, 'r')
     lines = f.readlines()
     num = len(lines)
@@ -133,17 +134,17 @@ def generator_val_batch(val_txt,batch_size,num_classes,img_path):
         for i in range(int(num / batch_size)):
             a = i * batch_size
             b = (i + 1) * batch_size
-            y_test,y_labels = process_batch(new_line[a:b],img_path,train=False)
+            y_test, y_labels = process_batch(new_line[a:b], img_path, train=False)
             x = preprocess(y_test)
-            x = np.transpose(x,(0,2,3,1,4))
-            y = np_utils.to_categorical(np.array(y_labels), num_classes)
+            x = np.transpose(x, (0, 2, 3, 1, 4))
+            y = np_utils.to_categorical(np.array(y_labels) - 1, num_classes)
             yield x, y
 
 
 def main():
-    img_path = '/home/tianz/datsets/ucfimgs/'
-    train_file = 'train_list.txt'
-    test_file = 'test_list.txt'
+    img_path = 'C:/Users/wahab/Desktop/FYP/data/'
+    train_file = 'train_list1.txt'
+    test_file = 'test_list1.txt'
     f1 = open(train_file, 'r')
     f2 = open(test_file, 'r')
     lines = f1.readlines()
@@ -153,23 +154,25 @@ def main():
     f2.close()
     val_samples = len(lines)
 
-    num_classes = 101
+    num_classes = 2
     batch_size = 16
     epochs = 16
 
     model = c3d_model()
     lr = 0.005
+
     sgd = SGD(lr=lr, momentum=0.9, nesterov=True)
     model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
     model.summary()
-    history = model.fit_generator(generator_train_batch(train_file, batch_size, num_classes,img_path),
-                                  steps_per_epoch=train_samples // batch_size,
-                                  epochs=epochs,
-                                  callbacks=[onetenth_4_8_12(lr)],
-                                  validation_data=generator_val_batch(test_file,
-                                        batch_size,num_classes,img_path),
-                                  validation_steps=val_samples // batch_size,
-                                  verbose=1)
+
+    history = model.fit_generator(generator_train_batch(train_file, batch_size, num_classes, img_path),
+                        steps_per_epoch=train_samples // batch_size,
+                        epochs=epochs,
+                        verbose=1,
+                        callbacks=[onetenth_4_8_12(lr)],
+                        validation_data=generator_val_batch(test_file, batch_size, num_classes, img_path),
+                        validation_steps=val_samples // batch_size)
+
     if not os.path.exists('results/'):
         os.mkdir('results/')
     plot_history(history, 'results/')
